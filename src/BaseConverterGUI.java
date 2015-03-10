f01afef98f4a3034c5e9bbca6df5ccbb4e50399f
xTEddie <t.eddie@live.com>
2015-03-09 21:46:19 -0400

    Added more keylisteners to JCombo objects

--- src/BaseConverterGUI.java
+++ src/BaseConverterGUI.java
@@ -32,25 +32,25 @@ import javax.swing.KeyStroke;
 
 @SuppressWarnings("serial")
 public class BaseConverterGUI extends JFrame implements KeyListener, MouseListener, FocusListener{
-	
+
 	public static final int WIDTH = 350;
 	public static final int HEIGHT = 270;
 	public static final String[] BASELIST = {"Base 2 (Binary)",
-											"Base 3 (Ternary",
-											"Base 4 (Quartenary)",
-											"Base 5 (Qinary)",
-											"Base 6 (Senary)",
-											"Base 7 (Septenary)",
-											"Base 8 (Octal)",
-											"Base 9 (Nonary)",
-											"Base 10 (Decimal)",
-											"Base 11 (Undecimal)",
-											"Base 12 (Duodecimal)",
-											"Base 13 (Tridecimal)",
-											"Base 14 (Tetradecimal)",
-											"Base 15 (Pentadecimal)",
-											"Base 16 (Hexadecimal)",
-											};
+		"Base 3 (Ternary",
+		"Base 4 (Quartenary)",
+		"Base 5 (Qinary)",
+		"Base 6 (Senary)",
+		"Base 7 (Septenary)",
+		"Base 8 (Octal)",
+		"Base 9 (Nonary)",
+		"Base 10 (Decimal)",
+		"Base 11 (Undecimal)",
+		"Base 12 (Duodecimal)",
+		"Base 13 (Tridecimal)",
+		"Base 14 (Tetradecimal)",
+		"Base 15 (Pentadecimal)",
+		"Base 16 (Hexadecimal)",
+	};
 	private JMenu converter;
 	private JMenu help;
 	private JComboBox<String> inputBox;
@@ -60,18 +60,20 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 	private String number = null;
 	private int inputBase = -1;
 	private int outputBase = -1;
-	
+	private boolean vk1_IsPressed = false;
+	long vk1Pressed;
+
 	public BaseConverterGUI() throws IOException{
 		super("Base^ Converter");
 		this.setSize(WIDTH, HEIGHT);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null); // Center window
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		// Menu
 		JMenuBar bar = new JMenuBar();
 		bar.setBackground(new Color(169,198,236)); 
-//		bar.setBackground(new Color(218,224,241));
+		//		bar.setBackground(new Color(218,224,241));
 		converter = new JMenu("Converter");
 		converter.setMnemonic('c'); // ALT + C shortcut
 		converter.addMouseListener(this);
@@ -83,13 +85,13 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 		quit.addActionListener(new QuitProgram());
 		JMenuItem about = new JMenuItem("Visit Website", KeyEvent.VK_V);
 		about.addActionListener(new OpenBrowser()); // Open website
-		
+
 		JPanel parentPane = new JPanel();
 		parentPane.setLayout(new BoxLayout(parentPane, 1));
 		parentPane.setBorder(BorderFactory.createEmptyBorder(10,8,10,8));
 		parentPane.setBackground(new Color(169,198,236)); 
 
-		
+
 		// Input Pane
 		JPanel inputPane = new JPanel();
 		inputPane.setBackground(new Color(169,198,236)); 
@@ -106,7 +108,7 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 		inputText.addKeyListener(new QuitProgram());
 		inputText.addKeyListener(this);
 		inputText.addFocusListener(this);
-		
+
 		// Output Pane
 		JPanel outputPane = new JPanel();
 		outputPane.setBackground(new Color(169,198,236)); 
@@ -121,13 +123,13 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 		outputValueLabel.setBorder(BorderFactory.createEmptyBorder(0,0,0,50));
 		outputText = new JTextField(15);
 		outputText.setEnabled(false);
-		
+
 		converter.add(quit);
 		help.add(about);
 		bar.add(converter);
 		bar.add(help);
 		this.setJMenuBar(bar);
-		
+
 		inputPane.add(inputLabel);
 		inputPane.add(inputBox);
 		inputPane.add(inputValueLabel);
@@ -147,6 +149,19 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 	@Override
 	public void keyPressed(KeyEvent e) {
 //		System.out.println(e);
+		if(e.getKeyCode() == KeyEvent.VK_1){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 200 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(9);
+				vk1_IsPressed = false;
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 200 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(9);
+				vk1_IsPressed = false;
+			}else{
+				vk1_IsPressed = true;
+			}
+			vk1Pressed = System.currentTimeMillis();
+		}
+
 		if(e.getKeyCode() == KeyEvent.VK_ALT){
 			e.consume(); // Remove default manner of alt key 
 			if(!converter.isOpaque() && !help.isOpaque()){
@@ -192,10 +207,84 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 			}else if(e.getSource().equals(outputBox)){
 				outputBase = outputBox.getSelectedIndex() + 2;
 			}
-			
+
 			// Convert
 			if(number != null && inputBase != -1 && outputBase != -1){	
 				outputText.setText(BaseConverter.convertBase(number, inputBase, outputBase));
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_2){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 100 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(10);	
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 1000 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(10);	
+			}else if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(0);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(0);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_3){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 100 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(11);	
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 1000 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(11);	
+			}else if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(1);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(1);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_4){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 100 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(12);	
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 1000 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(12);	
+			}else if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(2);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(2);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_5){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 100 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(13);	
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 1000 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(13);	
+			}else if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(3);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(3);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_6){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 100 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(14);	
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 1000 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(14);	
+			}else if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(4);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(4);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_7){
+			if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(5);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(5);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_8){
+			if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(6);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(6);
+			}
+		}else if(e.getKeyCode() == KeyEvent.VK_9){
+			if(e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(7);
+			}else if(e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(7);
+			}
+		}else if (e.getKeyCode() == KeyEvent.VK_0){
+			if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 100 && e.getSource().equals(inputBox)){
+				inputBox.setSelectedIndex(8);	
+			}else if(vk1_IsPressed && (System.currentTimeMillis()-vk1Pressed) <= 1000 && e.getSource().equals(outputBox)){
+				outputBox.setSelectedIndex(8);	
 			}
 		}
 	}
@@ -203,19 +292,18 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -227,7 +315,7 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 			help.setOpaque(true);
 			help.setBackground(new Color(218,224,241));
 		}
-		
+
 	}
 
 	@Override
@@ -244,24 +332,24 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 	@Override
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void focusGained(FocusEvent e) {
-		// TODO Auto-generated method stub
-		
+		//		System.out.println(e.getSource());
+
 	}
 
 	@Override
 	public void focusLost(FocusEvent e) {
-//		System.out.println(e);
+		//		System.out.println(e);
 		if(e.getSource().equals(inputBox)){
 			inputBase = inputBox.getSelectedIndex() + 2;
 		}else if(e.getSource().equals(inputText)){
@@ -269,11 +357,11 @@ public class BaseConverterGUI extends JFrame implements KeyListener, MouseListen
 		}else if(e.getSource().equals(outputBox)){
 			outputBase = outputBox.getSelectedIndex() + 2;
 		}
-		
+
 		// Convert
 		if(number != null && inputBase != -1 && outputBase != -1){
 			outputText.setText(BaseConverter.convertBase(number, inputBase, outputBase));
 		}
-			
+
 	}
 }
